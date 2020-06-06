40c6ccde5c44640f70ca9da4681a54068f07109d
Антон Ткачук <chiga2030@gmail.com>
2020-06-06 15:48:17 +0300

    откорректирован текст условиях проверки

--- commission.test.js
+++ commission.test.js
@@ -1,52 +1,67 @@
 const commission = require('./commission');
-const MILLISECONDS_IN_DAY = 24 * 60 * 60 * 1000;
-const SECOND = 1000;	// модификатор граничных значений (1 секунда)
+
+/*
+* 5 подклассов позитивных сценариев:
+* Класс 1: Время >= 10 дней - ожидаемая комиссия 0%
+* Класс 2: Время < 10 дней - ожидаемая комиссия 20%
+* Класс 3: Время < 5дней - ожидаемая комиссия 50%
+* Класс 4: Время < 24 часов - ожидаемая комиссия 75%
+* Класс 5: Время < 0 секунд - ожидаемая комиссия 100%
+*/
+
+// граничные значения:
+const value1 = 10; // Относится к классу 2 (10 дней)
+const value2 = 5; // Относится к классу 3 (5 дней)
+const value3 = 1; // Относится к классу 4 (24 часа)
+const value4 = 0; // Относится к классу 5 (0 секунд)
+const MILLISECONDS_IN_DAY = 24 * 60 * 60 * 1000; // Конвертация в милисекунды
+const SECOND = 1000;	// Модификатор граничного значения (1 секунда)
 
 // тест:
 	describe('Группа тестов для границы 10 дней', () => {
-			test('Граничное значение = 10 дней', () => {
-				  expect(commission(Date.now() + (10 * MILLISECONDS_IN_DAY))).toBe(0);
+			test('Если мы возвращаем билет ровно за 10 дней - ожидаем комисcию в 0%', () => {
+				  expect(commission(Date.now() + (value1 * MILLISECONDS_IN_DAY))).toBe(0);
 			});
-			test('Значение рядом с границей < 10 дней', () => {
-				  expect(commission(Date.now() + (10 * MILLISECONDS_IN_DAY) - SECOND)).toBe(20);
+			test('Если мы возвращаем билет менее, чем за 10 дней - ожидаем комисcию в 20%', () => {
+				  expect(commission(Date.now() + (value1 * MILLISECONDS_IN_DAY) - SECOND)).toBe(20);
 			});
-			test('Значение рядом с границей > 10 дней', () => {
-				  expect(commission(Date.now() + (10 * MILLISECONDS_IN_DAY) + SECOND)).toBe(0);
+			test('Если мы возвращаем билет более, чем за 10 дней - ожидаем комисcию в 0%', () => {
+				  expect(commission(Date.now() + (value1 * MILLISECONDS_IN_DAY) + SECOND)).toBe(0);
 			});
 	});
 
 	describe('Группа тестов для границы 5 дней', () => {
-			test('Граничное значение = 5 дней', () => {
-				  expect(commission(Date.now() + (5 * MILLISECONDS_IN_DAY))).toBe(20);
+			test('Если мы возвращаем билет ровно за 5 дней - ожидаем комисcию в 20%', () => {
+				  expect(commission(Date.now() + (value2 * MILLISECONDS_IN_DAY))).toBe(20);
 			});
-			test('Значение рядом с границей < 5 дней', () => {
-				  expect(commission(Date.now() + (5 * MILLISECONDS_IN_DAY) - SECOND)).toBe(50);
+			test('Если мы возвращаем билет менее, чем за 5 дней - ожидаем комисcию в 50%', () => {
+				  expect(commission(Date.now() + (value2 * MILLISECONDS_IN_DAY) - SECOND)).toBe(50);
 			});
-			test('Значение рядом с границей > 5 дней', () => {
-				  expect(commission(Date.now() + (5 * MILLISECONDS_IN_DAY) + SECOND)).toBe(20);
+			test('Если мы возвращаем билет более, чем за 5 дней - ожидаем комисcию в 20%', () => {
+				  expect(commission(Date.now() + (value2 * MILLISECONDS_IN_DAY) + SECOND)).toBe(20);
 			});
 	});
 
 	describe('Группа тестов для границы 24 часа', () => {
-			test('Граничное значение = 24 часа', () => {
-				  expect(commission(Date.now() + MILLISECONDS_IN_DAY)).toBe(50);
+			test('Если мы возвращаем билет ровно за 24 часа - ожидаем комисcию в 50%', () => {
+				  expect(commission(Date.now() + value3 * MILLISECONDS_IN_DAY)).toBe(50);
 			});
-			test('Значение рядом с границей < 24 часа', () => {
-				  expect(commission(Date.now() + MILLISECONDS_IN_DAY - SECOND)).toBe(75);
+			test('Если мы возвращаем билет менее, чем за 24 часа - ожидаем комисcию в 75%', () => {
+				  expect(commission(Date.now() + value3 * MILLISECONDS_IN_DAY - SECOND)).toBe(75);
 			});
-			test('Значение рядом с границей > 24 часа', () => {
-				  expect(commission(Date.now() + MILLISECONDS_IN_DAY + SECOND)).toBe(50);
+			test('Если мы возвращаем билет более, чем за 24 часа - ожидаем комисcию в 50%', () => {
+				  expect(commission(Date.now() + value3 * MILLISECONDS_IN_DAY + SECOND)).toBe(50);
 			});
 	});
 
 	describe('Группа тестов для границы 0 секунд', () => {
-			test('Граничное значение = 0 секунд', () => {
-				  expect(commission(Date.now())).toBe(75);
+			test('Если мы возвращаем билет ровно за 0 секунд - ожидаем комисcию в 75%', () => {
+				  expect(commission(Date.now() + value4 * MILLISECONDS_IN_DAY)).toBe(75);
 			});
-			test('Значение рядом с границей < 0 секунд', () => {
-				  expect(commission(Date.now() - SECOND)).toBe(100);
+			test('Если мы возвращаем билет менее, чем за 0 секунд - ожидаем комисcию в 100%', () => {
+				  expect(commission(Date.now() + value4 * MILLISECONDS_IN_DAY - SECOND)).toBe(100);
 			});
-			test('Значение рядом с границей > 0 секунд', () => {
-				  expect(commission(Date.now() + SECOND)).toBe(75);
+			test('Если мы возвращаем билет более, чем за 0 секунд - ожидаем комисcию в 75%', () => {
+				  expect(commission(Date.now() + value4 * MILLISECONDS_IN_DAY + SECOND)).toBe(75);
 			});
 	});
