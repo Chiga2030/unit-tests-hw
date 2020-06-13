a29710acaaa8d962c1f18879a8a09b4879ce60db
Антон Ткачук <chiga2030@gmail.com>
2020-06-13 16:35:45 +0300

    исправлены замечания по тестам

--- __tests__/passwordCheck.test.js
+++ __tests__/passwordCheck.test.js
@@ -4,35 +4,39 @@ const passwordCheck = require('../passwordCheck');
 * Классы эквивалентности:
 * Определим все негативные сценарии, так как их меньше чем позитивных.
 * Класс 1: Если в пароле отсутствуют числа
-* Класс 2: Если в пароле отсутствуют хотя бы одна заглавная и одна прописная буква английского алфавита
-* Класс 3: Если в пароле отсутствуют специальные символы /[!\?\.,\+\*\/\-]/
-* Класс 4: Если в пароле менне 10 символов
-* Класс 5: Позитивный сценарий - все остальные комбинации символов
+* Класс 2: Если в пароле отсутствуют хотя бы одна заглавная буква английского алфавита
+* Класс 3: Если в пароле отсутствуют хотя бы одна прописная буква английского алфавита
+* Класс 4: Если в пароле отсутствуют специальные символы /[!\?\.,\+\*\/\-]/
+* Класс 5: Если в пароле менне 10 символов
+* Класс 6: Позитивный сценарий - все остальные комбинации символов
 */
 
-// Значения для тестирования:
-const value1 = 'just a G**d password';
-const value2 = 'про100 Х*р*ший пароль';
-const value3 = 'just a Good pa55word';
-const value4 = 'jstG**pa5';
-const value5 = 'just a G**d pa55word';
-
 test('Проверка пароля на отсутствие чисел - ожидаем false', () => {
-			expect(passwordCheck(value1)).toBeFalsy();
-		});
+	const value1 = 'just a G**d password';
+	expect(passwordCheck(value1)).toBeFalsy();
+});
 
-test('Проверка пароля на отсутствие разнорегисторных букв английского алфавита - ожидаем false', () => {
-			expect(passwordCheck(value2)).toBeFalsy();
-		});
+test('Проверка пароля на отсутствие букв верхнего регистра английского алфавита - ожидаем false', () => {
+	const value2 = 'про100 х*р*ший пароль';
+	expect(passwordCheck(value2)).toBeFalsy();
+});
+
+test('Проверка пароля на отсутствие букв нижнего регистра английского алфавита - ожидаем false', () => {
+	const value3 = 'ПРО100 Х*Р*ШИЙ ПАРОЛЬ';
+	expect(passwordCheck(value3)).toBeFalsy();
+});
 
 test('Проверка пароля на отсутствие спецсимволов - ожидаем false', () => {
-			expect(passwordCheck(value3)).toBeFalsy();
-		});
+	const value4 = 'just a Good pa55word';
+	expect(passwordCheck(value4)).toBeFalsy();
+});
 
 test('Проверка пароля на минимальную длинну в 10 символов - ожидаем false', () => {
-			expect(passwordCheck(value4)).toBeFalsy();
-		});
+	const value5 = 'jstG**pa5';
+	expect(passwordCheck(value5)).toBeFalsy();
+});
 
 test('Проверка валидного пароля - ожидаем true', () => {
-			expect(passwordCheck(value5)).toBeTruthy();
-		});
+	const value6 = 'just a G**d pa55word';
+	expect(passwordCheck(value6)).toBeTruthy();
+});
--- __tests__/rle.test.js
+++ __tests__/rle.test.js
@@ -8,26 +8,20 @@ const rle = require('../rle');
 * Класс 3: Позитивный сценарий - любая последовательность символов алфавита, от одного знака и более
 */
 
-// Тестируемые значения:
-const value1 = 'ABCABCABC';
-const value2 = 'AAABBBCCC';
-const value3 = 'AAABCCCDEFFFFFFF';
-
-test('Проверка одинаковых последовательностей седующих друг за другом, по патерну /^[A-Z]+$/', () => {
-		const res = 'ABCABCABC';
-	  expect(rle(value1)).toBe(res);
-	});
+test('Проверка одинаковых последовательностей следующих друг за другом, по патерну /^[A-Z]+$/', () => {
+	const value1 = 'ABCABCABC';
+	const res = 'ABCABCABC';
+  expect(rle(value1)).toBe(res);
+});
 
 test('Проверка входящих параметров по патерну /^[A-Z]+$/', () => {
-		const res = 'A3B3C3';
-	  expect(rle(value2)).toBe(res);
-	});
+	const value2 = 'AAABBBCCC';
+	const res = 'A3B3C3';
+  expect(rle(value2)).toBe(res);
+});
 
 test('Проверка рандомной последовательности входящих параметров по патерну /^[A-Z]+$/', () => {
-		const res = 'A3BC3DEF7';
-	  expect(rle(value3)).toBe(res);
-	});
-
-/**
-* P.S. вызов rle('A999') - вернет А93, что искозит исходные данные, такой тест в идеале должен возвращать ошибку из-за присутствия цифр во входящем параметре.
-*/
+	const value3 = 'AAABCCCDEFFFFFFF';
+	const res = 'A3BC3DEF7';
+  expect(rle(value3)).toBe(res);
+});
