3f7008bb83a3c3be78312c007c2e230e72367d28
Антон Ткачук <chiga2030@gmail.com>
2020-06-14 21:16:34 +0300

    start testing

--- __tests__/getIntersection.test.js
+++ __tests__/getIntersection.test.js
@@ -1,5 +1,5 @@
 const getIntersection = require('../getIntersection');
 
 test('test', () => {
-	excepted(getIntersection([1, 3, 5, 7, 9], [1, 2, 3, 4]).toBe([1, 3]));
+	expect(getIntersection([1, 3, 5, 7, 9], [1, 2, 3, 4])).toEqual([1, 3]);
 });
